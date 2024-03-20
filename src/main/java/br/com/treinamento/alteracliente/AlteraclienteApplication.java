3214ac47424b569f9defb43bbd474053dd305317
AmauriFernandes <amaurif75@gmail.com>
2024-03-20 09:32:11 -0300

    initial commit

---
+++ src/main/java/br/com/treinamento/alteracliente/AlteraclienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.alteracliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlteraclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlteraclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamento/alteracliente/AlteraclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.alteracliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlteraclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
